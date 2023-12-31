4bcd6c0658ff98c97f52a7a9885612c276dcdfdc
MitchSmoot <mitchsmoot@gmail.com>
2023-12-30 19:37:50 -0700

    configured harpoon

--- init.lua
+++ init.lua
@@ -249,7 +249,9 @@ require('lazy').setup({
 
   -- Harpoon --
   {
-    'ThePrimeagen/harpoon'
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = {'nvim-lua/plenary.nvim'}
   },
 
   {
@@ -663,6 +665,26 @@ cmp.setup {
     { name = 'path' },
   },
 }
+
+-- Harpoon setup
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+-- End Harpoon Setup
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

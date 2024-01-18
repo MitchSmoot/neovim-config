6a4c161d6d4cf2e0b0091b13fbecfcb50a63de6c
MitchSmoot <mitchsmoot@gmail.com>
2024-01-18 14:56:10 -0700

    added more keybinds

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,18 +1,25 @@
 vim.g.mapleader = " "
 
+local keymap = vim.keymap
+local harpoon = require("harpoon")
 
 -- Harpoon Keymaps
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+harpoon:setup()
+keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
+keymap.set("n", "<leader>6", function() harpoon:list():select(6) end)
+keymap.set("n", "<leader>7", function() harpoon:list():select(7) end)
+keymap.set("n", "<leader>8", function() harpoon:list():select(8) end)
+keymap.set("n", "<leader>9", function() harpoon:list():select(9) end)
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
+keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
 
 -- Oil Keymaps
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
